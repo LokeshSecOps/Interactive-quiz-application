4c2ebcd30646cd162a50635e69d1656910b95bcc
LokeshSecOps <Lokeshsharma02110@gmail.com>
2026-06-16 22:18:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const questions = [
     question: "What is the capital of France?",
     options: ["Berlin", "Madrid", "Paris", "Rome"],
     answer: "Paris" 
-  }, 
+  },  
   { 
     question: "Which language runs in a web browser?",
     options: ["Python", "C", "JavaScript", "Java"],
